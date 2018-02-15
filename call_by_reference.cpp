d7a90b8861cb8de24ca6a90f4b15839827637c80
stephen <stevie.barbera@gmail.com>
2018-02-14 20:25:00 -0600

    lab1

---
+++ call_by_reference.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void add_numbers(int& x, int y)
+{
+	x += y;
+}
+
+int main()
+{
+	int first;
+	int second;
+
+	cout << "Enter first number: ";
+	cin >> first;
+	cout << "Enter second number: ";
+	cin >> second;
+
+	add_numbers(first, second);
+	cout << "The first time calling add_numbers is: " << first << endl;
+	
+	add_numbers(first, second);
+	cout << "The second time calling add_numbers is: " << first << endl;
+
+	add_numbers(first, second);
+	cout << "The third time calling add_numbers is: " << first << endl;
+}
